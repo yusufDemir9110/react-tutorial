53ac68f7a2a864f2f5ec74adb3b96e3f0a2926d5
yusuf Demir <yusuf.ferrum@gmail.com>
2021-11-26 09:00:34 +0100

    2 saat 51 dakika

--- tutorial2/src/components/meetups/NewMeetupForm.js
+++ tutorial2/src/components/meetups/NewMeetupForm.js
@@ -1,25 +1,45 @@
 import Card from '../ui/Card'
 import classes from './NewMeetupForm.module.css'
+import {useRef} from 'react'
 
-function NewMeetupForm(){
+function NewMeetupForm(props){
+const titleInputRef= useRef()
+const imageInputRef = useRef()
+const addressInputRef = useRef()
+const descriptionInputRef = useRef()
+    function submitHandler(event){
+        event.preventDefault()
+        const enteredTitle = titleInputRef.current.value
+        const enteredImage=imageInputRef.current.value
+        const enteredAddress=addressInputRef.current.value
+        const enteredDescription= descriptionInputRef.current.value
+
+        const meetupData={
+            title:enteredTitle,
+            image:enteredImage,
+            address:enteredAddress,
+            description:enteredDescription
+        }
+        props.onAddMeetup(meetupData)
+    }
     return (
         <Card>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={submitHandler}>
                 <div className={classes.control}>
                     <label htmlFor='title'>Meetup title</label>
-                    <input type='text' required id='title'></input>
+                    <input type='text' required id='title' ref={titleInputRef}></input>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='image'>Meetup Image</label>
-                    <input type='url' required id='image'></input>
+                    <input type='url' required id='image' ref={imageInputRef}></input>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='address'>Address</label>
-                    <input type='text' required id='address'></input>
+                    <input type='text' required id='address' ref={addressInputRef}></input>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='description'>Description</label>
-                    <textarea id='description' required rows='5'></textarea>
+                    <textarea id='description' required rows='5' ref={descriptionInputRef}></textarea>
                 </div>
                 <div className={classes.actions}>
                     <button>Add Meetup</button>
--- tutorial2/src/pages/NewMeetUp.js
+++ tutorial2/src/pages/NewMeetUp.js
@@ -1,11 +1,24 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage(){
+    function addMeetupHandler(meetupData){
+        fetch(
+            'https://react-academind-be972-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+            {
+                method:"POST",
+                body:JSON.stringify(meetupData),
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            }
+            )
+
+    }
     return <section>
         <h1>
             Add new meetup
         </h1>
-        <NewMeetupForm/>
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
 }
 export default NewMeetupPage;
